730f6e14a6391c41d4665634228df2883634a949
Piotr Kocia <p.kocia@wp.pl>
2021-10-09 00:17:44 +0200

    Update WWO_01_11.cpp

--- WWO_01_11.cpp
+++ WWO_01_11.cpp
@@ -1,26 +1,76 @@
+#include <stdio.h>
 #include <iostream>
 #include <array>
 #include <vector>
 
 using i64 = long long;
 
+static i64 numbers[1000000] = {};
+static i64 counts[1000000] = {};
+static i64 const powers[19] = {387420489, 129140163, 43046721, 14348907, 4782969, 1594323, 531441, 177147, 59049, 19683, 6561, 2187, 729, 243, 81, 27, 9, 3, 1};
+
+struct IO_Buffer {
+    char buf[65536];
+    std::basic_streambuf<char, std::char_traits<char>>* rdbuf;
+    char* i;
+    char* end;
+
+    IO_Buffer(): rdbuf(std::cin.rdbuf()), i(buf), end(buf) {}
+
+    char get() {
+        if(i == end) {
+            i64 count = rdbuf->sgetn(buf, 65536);
+            i = buf;
+            end = buf + count;
+        }
+
+        if(i == end) {
+            return EOF;
+        }
+
+        char c = *i;
+        ++i;
+        return c;
+    }
+
+    i64 read_i64() {
+        char c = get();
+        while((c < '0' || c > '9') && c != EOF) {
+            c = get();
+        }
+
+        if(c == EOF) {
+            return -1;
+        }
+
+        i64 v = 0;
+        while(c >= '0' && c <= '9') {
+            v *= 10;
+            v += c - '0';
+            c = get();
+        }
+
+        return v;
+    }
+};
+
 int main() {
     std::ios_base::sync_with_stdio(false);
-    i64 t = 0;
-    std::cin >> t;
-    std::array<i64, 19> powers = {387420489, 129140163, 43046721, 14348907, 4782969, 1594323, 531441, 177147, 59049, 19683, 6561, 2187, 729, 243, 81, 27, 9, 3, 1};
-    std::vector<i64> numbers(t, 0);
-    std::vector<i64> counts(t, 0);
-    for(i64* i = numbers.data(), *end = numbers.data() + t; i != end; ++i) {
-        std::cin >> *i;
+    setvbuf(stdout, nullptr, _IOFBF, 65365);
+    IO_Buffer io_buffer;
+    i64 t = io_buffer.read_i64();
+    i64* const c_b = counts;
+    i64* const c_e = counts + t;
+    i64* const n_b = numbers;
+    i64* const n_e = numbers + t;
+    for(i64* n = n_b, *end = n_e; n != end; ++n) {
+        *n = io_buffer.read_i64();
     }
 
     for(i64 p: powers) {
-        i64* n = numbers.data();
-        i64* n_end = numbers.data() + t;
-        i64* c = counts.data();
-        i64* c_end = counts.data() + t;
-        for(; n != n_end; ++n, ++c) {
+        i64* n = n_b;
+        i64* c = c_b;
+        for(; n != n_e; ++n, ++c) {
             // if(*n >= p) {
             //     *n -= p;
             //     *c += 1;
@@ -31,8 +81,11 @@ int main() {
         }
     }
     
-    for(i64 c: counts) {
-        std::cout << c << '\n';
+    char buf[4] = {'1', '0', '\0'};
+    for(i64* c = c_b; c != c_e; ++c) {
+        bool offset = *c < 10;
+        buf[1] = *c % 10 + '0';
+        puts(buf + offset);
     }
 
     return 0;
