c93f5986709f157714ea3c88448731168df1176d
Piotr Kocia <p.kocia@wp.pl>
2021-10-09 23:52:01 +0200

    Update WWO_01_11.cpp

--- WWO_01_11.cpp
+++ WWO_01_11.cpp
@@ -1,91 +1,124 @@
 #include <stdio.h>
-#include <iostream>
-#include <array>
-#include <vector>
 
 using i64 = long long;
+using u8 = unsigned char;
 
-static i64 numbers[1000000] = {};
-static i64 counts[1000000] = {};
-static i64 const powers[19] = {387420489, 129140163, 43046721, 14348907, 4782969, 1594323, 531441, 177147, 59049, 19683, 6561, 2187, 729, 243, 81, 27, 9, 3, 1};
-
-struct IO_Buffer {
+struct Input_Buffer {
     char buf[65536];
-    std::basic_streambuf<char, std::char_traits<char>>* rdbuf;
     char* i;
     char* end;
 
-    IO_Buffer(): rdbuf(std::cin.rdbuf()), i(buf), end(buf) {}
-
-    char get() {
-        if(i == end) {
-            i64 count = rdbuf->sgetn(buf, 65536);
-            i = buf;
-            end = buf + count;
-        }
-
-        if(i == end) {
-            return EOF;
-        }
-
-        char c = *i;
-        ++i;
-        return c;
+    Input_Buffer(): i(buf), end(buf) {
+        buf[65535] = EOF;
     }
 
     i64 read_i64() {
-        char c = get();
-        while((c < '0' || c > '9') && c != EOF) {
-            c = get();
+        if(end - i < 10) {
+            i64 remaining = end - i;
+            char* b = i;
+            i = buf;
+            while(b != end) {
+                *i = *b;
+                ++i;
+                ++b;
+            }
+
+            i64 count = fread(i, 1, 65535 - remaining, stdin);
+            i = buf;
+            end = buf + remaining + count;
         }
 
-        if(c == EOF) {
-            return -1;
+        while(*i > '9' | *i < '0') {
+            ++i;
         }
 
         i64 v = 0;
-        while(c >= '0' && c <= '9') {
+        while(*i >= '0' & *i <= '9') {
             v *= 10;
-            v += c - '0';
-            c = get();
+            v += *i - '0';
+            ++i;
         }
 
         return v;
     }
 };
 
-int main() {
-    std::ios_base::sync_with_stdio(false);
-    setvbuf(stdout, nullptr, _IOFBF, 65365);
-    IO_Buffer io_buffer;
-    i64 t = io_buffer.read_i64();
-    i64* const c_b = counts;
-    i64* const c_e = counts + t;
-    i64* const n_b = numbers;
-    i64* const n_e = numbers + t;
-    for(i64* n = n_b, *end = n_e; n != end; ++n) {
-        *n = io_buffer.read_i64();
+struct Output_Buffer {
+    char buf[65536];
+    char* i;
+    char* end;
+
+    Output_Buffer(): i(buf), end(buf + 65536) {}
+
+    ~Output_Buffer() {
+        fwrite(buf, 1, i - buf, stdout);
     }
 
-    for(i64 p: powers) {
-        i64* n = n_b;
-        i64* c = c_b;
-        for(; n != n_e; ++n, ++c) {
-            // if(*n >= p) {
-            //     *n -= p;
-            //     *c += 1;
-            // }
-            bool g = *n >= p;
-            *n -= p * g;
-            *c += g;
+    void write_i64(i64 v) {
+        if(end - i < 3) {
+            fwrite(buf, 1, i - buf, stdout);
+            i = buf;
         }
+
+        bool offset = v >= 10;
+        i[0] = '1';
+        i[0 + offset] = v % 10 + '0';
+        i[1 + offset] = '\n';
+        i += 2 + offset;
     }
-    
-    char buf[4] = {'1', '0', '\0'};
-    for(i64* c = c_b; c != c_e; ++c) {
-        bool offset = *c < 10;
-        buf[1] = *c % 10 + '0';
-        puts(buf + offset);
+};
+
+int main() {
+    // setvbuf(stdin, nullptr, _IONBF, 1);
+    // setvbuf(stdout, nullptr, _IONBF, 1);
+    Input_Buffer inbuf;
+    Output_Buffer outbuf;
+    i64 t = inbuf.read_i64();
+    u8 ter_buf[32] = {};
+
+    for(; t > 0; --t) {
+        i64 n = inbuf.read_i64();
+        i64 i = 0;
+        i64 ones = 0;
+        i64 two_pos = -1;
+        for(; n > 0; i += 4) {
+            ter_buf[i] = n % 3;
+            ter_buf[i + 1] = (n / 3) % 3;
+            ter_buf[i + 2] = (n / 9) % 3;
+            ter_buf[i + 3] = (n / 27) % 3;
+            n /= 81;
+
+            if(ter_buf[i] == 2) {
+                two_pos = i;
+                ones = 0;
+            } else {
+                ones += ter_buf[i];
+            }
+
+            if(ter_buf[i + 1] == 2) {
+                two_pos = i + 1;
+                ones = 0;
+            } else {
+                ones += ter_buf[i + 1];
+            }
+
+            if(ter_buf[i + 2] == 2) {
+                two_pos = i + 2;
+                ones = 0;
+            } else {
+                ones += ter_buf[i + 2];
+            }
+
+            if(ter_buf[i + 3] == 2) {
+                two_pos = i + 3;
+                ones = 0;
+            } else {
+                ones += ter_buf[i + 3];
+            }
+        }
+
+        i64 c = ones + two_pos + 1;
+        outbuf.write_i64(c);
     }
 
     return 0;
